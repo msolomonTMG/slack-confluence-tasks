40dd46d10f6e76baa8ca1f6dd0b4ebcb9891baed
Mike Solomon <msolomon@thrillist.com>
2017-05-16 00:19:37 -0400

    group attachments by page

--- slackbot/slackbot.js
+++ slackbot/slackbot.js
@@ -32,21 +32,32 @@ var helpers = {
     let attachments = []
     return new Promise(function(resolve, reject) {
       tasks.forEach((task, index) => {
-        attachments.push({
-          fallback: task.title,
-          fields: [
-            {
-              title: "Task",
-              value: task.title,
-              short: true
-            },
-            {
-              title: "Due Date",
-              value: task.date,
-              short: true
-            }
-          ]
-        })
+        let titleField = {
+          title: "Task",
+          value: task.title,
+          short: true
+        }
+        let dateField = {
+          title: "Due Date",
+          value: task.date,
+          short: true
+        }
+
+        // group slack attachments by confluence page
+        let existingAttachments = attachments.filter(attachment => {
+          return attachment.title_link == `${process.env.CONFLUENCE_URL}${task.item.url}`;
+        });
+
+        if (existingAttachments.length > 0) {
+          existingAttachments[0].fields.push(titleField, dateField)
+        } else {
+          attachments.push({
+            fallback: task.title,
+            title: task.item.title,
+            title_link: `${process.env.CONFLUENCE_URL}${task.item.url}`,
+            fields: [ titleField, dateField ]
+          })
+        }
         if (index + 1 == tasks.length) {
           return resolve(attachments)
         }
@@ -59,7 +70,6 @@ var functions = {
   postMessageToUser: function(username, msg) {
     return new Promise(function(resolve, reject) {
       bot.postMessageToUser(username, msg, function(data) {
-        console.log(data)
         return resolve(data)
       });
     });
@@ -67,18 +77,15 @@ var functions = {
   sendTasksToUser: function(username, tasks) {
     return new Promise(function(resolve, reject) {
       helpers.formatTasks(tasks).then(formattedTasks => {
-        console.log(formattedTasks)
+
         helpers.transformTasksIntoAttachments(formattedTasks).then(attachments => {
           let params = {}
           params.attachments = JSON.stringify(attachments)
 
-          bot.postMessageToUser(username, formattedTasks[0].title, params, function(data) {
+          bot.postMessageToUser(username, ':memo: Here are your open Confluence Tasks', params, function(data) {
             return resolve(data)
           })
         })
-        // helpers.transformTasksIntoAttachments(formattedTasks).then(attachments => {
-        //
-        // })
 
       })
     });
